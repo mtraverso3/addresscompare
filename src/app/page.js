af22645e91bcb2bde08c6dcc95b07eb033216917
Marcos Traverso <marcostraverso2003@gmail.com>
2024-04-20 17:23:02 -0400

    Add footer

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,10 @@ export default function Home() {
     return (
         <div className="flex h-screen justify-center items-center">
             <AddressCompare/>
+            <footer className="absolute bottom-0 right-0 p-4">
+                Source code <a href="https://github.com/mtraverso3/addresscompare"
+                               className="text-blue-500 hover:text-blue-700"> available here</a>
+            </footer>
         </div>
     );
 }
